2de2ab767ec67522991b1f9bf519a5b2aa5b9e82
Siddhi Degoankar <siddhidegaonkar@gmail.com>
2018-01-29 02:07:51 +0530

    first commit

--- ecommerce/db/migrate/20180122044113_create_carts.rb
+++
@@ -1,9 +0,0 @@
-class CreateCarts < ActiveRecord::Migration[5.1]
-  def change
-    create_table :carts do |t|
-      t.float :total_price
-      t.integer :count
-      t.timestamps
-    end
-  end
-end
--- ecommerce/db/migrate/20180122060235_add_price_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToItems < ActiveRecord::Migration[5.1]
-  def change
-    add_column :items, :price, :float
-  end
-end
